a037c6f656031754db01213dec32ef5be3b59bb4
Sham Shivani <shamsundersuther@gmail.com>
2022-12-14 20:40:02 +0500

    how to gitignore

---
+++ index.js
@@ -0,0 +1,24 @@
+// Make basic server output in browser
+
+// const http = require("http");
+// var response = (req, res) => {
+//   res.write("<h1> Hello, There how are you </h1>");
+//   res.end();
+// };
+// http.createServer(response).listen(4500);
+
+// Ctrl + C to stop the server
+
+
+// what is package.json files and its use?
+// package.json stores all the info about the project.
+// package-lock.json stores all the info about the dependencies/packages 
+
+const colors = require("colors");
+console.log("hello there".bgRed);
+
+// Question: what if node modules is deleted accidentally?
+// Answer: You can run the "npm install" command to reinstall it
+
+// What is the correct way to push code to github?
+// Answer:
